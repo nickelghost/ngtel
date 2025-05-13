73ee5b2fdee3efa0709dda447247db3bb9e1dacd
Gabriel Karczewski <nickelghost@outlook.com>
2025-05-13 19:57:05 +0200

    add function that determines the log level from string

--- log.go
+++ log.go
@@ -8,6 +8,24 @@ import (
 	"strings"
 )
 
+// GetLogLevel determines the slog log level based on the provided string.
+// It returns slog.LevelInfo if the provided string does not match any known level.
+// The function is case-insensitive and supports the following levels: debug, info, warn, error.
+func GetLogLevel(level string) slog.Level {
+	switch strings.ToLower(level) {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	}
+
+	return slog.LevelInfo
+}
+
 // SetUpLogger sets the default logger as that of the chosen format.
 func SetUpLogger(w io.Writer, format string, lvl slog.Level) {
 	opts := &slog.HandlerOptions{Level: lvl, AddSource: true}
