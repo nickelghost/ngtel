a335f477bc36b0bdccff161f0307b1f3ff1994ef
Gabriel Karczewski <nickelghost@outlook.com>
2025-09-14 16:19:28 +0200

    make it vendor-neutral and opentel only

--- trace.go
+++ trace.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log/slog"
 
-	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
-	"go.opentelemetry.io/contrib/detectors/gcp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -14,42 +12,37 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.opentelemetry.io/otel/trace"
 	"go.opentelemetry.io/otel/trace/noop"
-	"google.golang.org/api/option"
 )
 
-// CreateCloudTracer gives us a fully set up tracer for usage in Google Cloud.
+// CreateTracer gives us a tracer without an exporter.
 // It also gives us a dummy tracer in case of the logging not being enabled.
 // The shutdown function should be called when the application is shutting down to ensure all traces are sent.
-func CreateCloudTracer(
-	ctx context.Context, enabled bool, gcpProject string, serviceName string,
+func CreateTracer(
+	ctx context.Context,
+	enabled bool,
+	serviceName string,
+	resOpts []resource.Option,
+	tpOpts []sdktrace.TracerProviderOption,
 ) (trace.Tracer, func(), error) {
 	if !enabled {
 		return noop.NewTracerProvider().Tracer("main"), func() {}, nil
 	}
 
-	exporter, err := texporter.New(
-		texporter.WithProjectID(gcpProject),
-		texporter.WithTraceClientOptions([]option.ClientOption{option.WithTelemetryDisabled()}),
-	)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to create trace exporter: %w", err)
-	}
-
-	res, err := resource.New(ctx,
-		resource.WithDetectors(gcp.NewDetector()),
+	res, err := resource.New(ctx, append(
+		resOpts,
 		resource.WithTelemetrySDK(),
 		resource.WithAttributes(
 			semconv.ServiceNameKey.String(serviceName),
 		),
-	)
+	)...)
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to create resource: %w", err)
 	}
 
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exporter),
+	tp := sdktrace.NewTracerProvider(append(
+		tpOpts,
 		sdktrace.WithResource(res),
-	)
+	)...)
 
 	otel.SetTextMapPropagator(
 		propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
@@ -62,15 +55,4 @@ func CreateCloudTracer(
 	}
 
 	return tp.Tracer("main"), shutdown, nil
-}
-
-// GetCloudTracePath gives us the path identifier of our current trace, enabling us to connect it in logs for example.
-// It returns an empty string if the trace ID is not set.
-func GetCloudTracePath(ctx context.Context) string {
-	sc := trace.SpanContextFromContext(ctx)
-	if sc.HasTraceID() {
-		return fmt.Sprintf("projects/%s/traces/%s", GetGCPProject(ctx), sc.TraceID().String())
-	}
-
-	return ""
 }
